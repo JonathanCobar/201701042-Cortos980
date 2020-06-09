63473f10130b8e948cb96758bc1c5e2786b938d2
usac.201701042 <3232499730509@ingenieria.usac.edu.gt>
2020-06-09 12:27:34 -0600

    se añadieron más comentarios

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -3,26 +3,28 @@
 
 # #se define la función que calcula el siguiente numero de la seucencia
 def calculo_N(num):
-    lista=[num]
+    lista=[num]#se añade el numero inicial en la primera posición de la lista
+    #se crea el ciclo que genera el siguiente numero siempre y cuando no se llegue a 1
     while num!=1:  
         if num%2==0:
             num=num/2
         else:
             num=(3*num)+1
-        lista.append(int(num))
-    return lista
-
+        lista.append(int(num))#añade el número generado a la lista
+    return lista#devuelve la secuencia
+#crea la función para agregar texto, el archivo por defecto es collatz.txt
+#a menos que se indique lo contrario, y el parametro que siempre se debe de dar
+#es lo que se va a añadir al texto
 def agregar_texto(lista, archivo="collatz.txt"):
-    documento=open(archivo, "a")
-    documento.write(lista+"\n")
+    documento=open(archivo, "a")#se usa a para que no reescriba todo y se quede con una linea
+    documento.write(lista+"\n")#se escribe un enter para que esté ordenado
     documento.close()
-
+#se limpia el documento para que no se acumulen todas las ejecuciones
 documento=open("collatz.txt", "w")
 documento.write("")
 documento.close()
 #se pone 42+1 porque range genera 42 numeros incluyendo el 2
 #por lo que termina en 41
-
 for i in range(2,(42+1)):
     secuencia=str(calculo_N(i))
     agregar_texto(secuencia)
